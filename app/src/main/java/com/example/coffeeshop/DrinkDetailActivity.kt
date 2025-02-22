e8b61b012e5c72855f6a163e0d440ed982377d3a
helloMinh <phandinhminh48@gmail.com>
2025-02-23 02:41:16 +0700

    Update

--- app/src/main/java/com/example/coffeeshop/DrinkDetailActivity.kt
+++ app/src/main/java/com/example/coffeeshop/DrinkDetailActivity.kt
@@ -1,8 +1,11 @@
 package com.example.coffeeshop
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -33,8 +36,59 @@ class DrinkDetailActivity : AppCompatActivity() {
         }
 
         binding.btnUpdate.setOnClickListener {
+            openUpdateDialog(
+                intent.getStringExtra("drinkId").toString(),
+                intent.getStringExtra("drinkName").toString(),
+                intent.getStringExtra("drinkType").toString(),
+                intent.getStringExtra("drinkPrice").toString()
+            )
         }
     }
+
+    private fun openUpdateDialog(id: String, name: String, type: String, price: String) {
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.update_dialog, null)
+        mDialog.setView(mDialogView)
+
+        // update thong tin cho dialog
+        val nameEditText = mDialogView.findViewById<EditText>(R.id.nameEditText)
+        val typeEditText = mDialogView.findViewById<EditText>(R.id.typeEditText)
+        val priceEditText = mDialogView.findViewById<EditText>(R.id.priceEditText)
+        nameEditText.setText(name)
+        typeEditText.setText(type)
+        priceEditText.setText(price)
+
+
+        mDialog.setTitle("Updating $name Record")
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        // click update
+        val updateButton = mDialogView.findViewById<Button>(R.id.saveButton)
+        updateButton.setOnClickListener {
+            updateDrinkData(
+                id,
+                nameEditText.text.toString(),
+                typeEditText.text.toString(),
+                priceEditText.text.toString()
+            )
+        }
+        // click cancel
+        val cancelButton = mDialogView.findViewById<Button>(R.id.cancelButton)
+        cancelButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+    }
+
+    private fun updateDrinkData(id: String, name: String, type: String, price: String) {
+        val dbRef = FirebaseDatabase.getInstance().getReference("Drinks").child(id)
+        val drinkInfo = DrinkModel(id, name, type, price)
+        dbRef.setValue(drinkInfo)
+        Toast.makeText(this, "Update success", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
 
     private fun deleteRecord(id: String) {
         val dbRef = FirebaseDatabase.getInstance().getReference("Drinks").child(id)
